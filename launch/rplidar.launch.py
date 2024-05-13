2658ca49ada3aeb5e06af71cc1840f88b9c7cd7d
swethag <swethag@ubuntu.myguest.virtualbox.org>
2024-05-13 13:29:59 +0530

    idk

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -9,13 +9,12 @@ def generate_launch_description():
         Node(
             package='rplidar_ros',
             executable='rplidar_composition',
+            output='screen',
             parameters=[{
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
-                'serial_baudrate':115200,
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
-                'scan_mode': 'Standard',
+                'scan_mode': 'Standard'
             }]
-            output='screen'
         )
     ])
